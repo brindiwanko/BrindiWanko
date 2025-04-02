16d0926ba8f66ca19c7be713818087bd73bc98f3
Gildo Duarte <gildoduarte@gmail.com>
2025-04-02 23:59:22 +0100

    Added functional test for execution time

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/hello-world', name: 'app_hello_world')]
 final class HelloWorldController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(
+        Request $request,
+    ): Response
     {
-        return $this->render('hello_world/index.html.twig');
+        return $this->render('hello_world/index.html.twig', [
+            'execution_time' => $request->attributes->get('execution_time'),
+        ]);
     }
 }
