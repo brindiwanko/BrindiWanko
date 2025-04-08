d4930fd0f5a2d8ca1b78e0b66e3fe39a1f3c19b5
brindiwanko <mycaranille@gmail.com>
2025-04-08 13:44:38 +0200

    fix

--- migrations/Version20250408114420.php
+++ migrations/Version20250408114420.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250408113937 extends AbstractMigration
+final class Version20250408114420 extends AbstractMigration
 {
     public function getDescription(): string
     {
