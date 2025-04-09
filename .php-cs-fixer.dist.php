0c2edeb9604268141d447fed2b0ab3927e04ff15
Master GHD <gildoduarte@gmail.com>
2025-04-09 20:28:21 +0100

    Added optimized configuration for symfony 7.2 + exclusions

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,26 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('var')
+    ->ignoreVCSIgnored(true)
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'cast_spaces' => ['space' => 'single'],
+        'concat_space' => ['spacing' => 'one'],
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_align' => ['align' => 'vertical'],
+        'phpdoc_order' => true,
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
     ])
-    ->setFinder($finder)
-;
+    ->setRiskyAllowed(true)
+    ->setFinder($finder);
