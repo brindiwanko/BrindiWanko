93cc74a18853a1169221f8e7f5d41da0b2c1fabc
Master GHD <gildoduarte@gmail.com>
2025-04-09 22:38:36 +0100

    Optimization for php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,24 +5,23 @@ declare(strict_types=1);
 set_time_limit(3600);
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-    ->exclude('vendor')
-    ->exclude('node_modules')
-    // exclude special files in config
-    ->exclude('config/bundles.php')
-    ->exclude('config/preload.php')
-    // special files used by project
-    ->exclude('public/index.php')
-    ->exclude('importmap.php')
-    // exclude tool
-    ->exclude('public/adminer.php')
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/migrations',
+    ])
     ->ignoreVCSIgnored(true)
 ;
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(
+        PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect()
+    )
     ->setRules([
         '@Symfony' => true,
+        '@PSR12' => true,
+        'array_indentation' => true,
+        'combine_consecutive_unsets' => true,
         'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
         'blank_line_after_namespace' => true,
@@ -31,8 +30,12 @@ return (new PhpCsFixer\Config())
         'concat_space' => ['spacing' => 'one'],
         'declare_strict_types' => true,
         'no_unused_imports' => true,
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['const', 'class', 'function'],
+        ],
         'phpdoc_align' => ['align' => 'vertical'],
+        'no_useless_else' => true,
         'phpdoc_order' => true,
         'single_quote' => true,
         'trailing_comma_in_multiline' => ['elements' => ['arrays']],
