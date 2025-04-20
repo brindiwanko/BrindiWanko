fe6c064c66f801fac87f6d957bd6bfdfa42285a8
Caranille <caranille@outlook.fr>
2025-04-20 19:58:28 +0200

    fix

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -7,7 +7,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-x
+
 final class AboutController extends AbstractController
 {
     #[Route('/about', name: 'app_about')]
